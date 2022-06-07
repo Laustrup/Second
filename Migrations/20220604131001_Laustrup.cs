e2f00e12330ecec98244b3dac7db26e5b5a7d9a0
Laust <laust.bonnesen@mail.com>
2022-06-07 11:47:00 +0200

    Migrations

---
+++ Migrations/20220604131001_Laustrup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webshop.Migrations
+{
+    public partial class Laustrup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "Products");
+        }
+    }
+}
